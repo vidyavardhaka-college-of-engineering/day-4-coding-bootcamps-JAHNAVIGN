1ddf2a3a47e53d50ce3494cd13a6a6228a43857e
JAHNAVIGN <20CSE0218@VVCE.AC.IN>
2021-02-19 10:56:03 +0000

    got to learn about using for loop to print max and min integer

--- Program_5.c
+++ Program_5.c
@@ -5,13 +5,23 @@
 
 int main()
 {
-    int arr[10], n, i;
+    int arr[10], n, i,min,max;
     scanf("%d", &n);
 
     for(i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    //Write your code here
-  
+    max = arr[0];
+    min = arr[0];
+
+    for(i = 1; i< n;i++)
+    {
+      if (arr[i]> max)
+      max =arr[i];
+
+      if(arr[i]< min)
+      min =arr[i];
+    }
+    printf("max = %d\nmin = %d",max,min);
     return 0;
 }
